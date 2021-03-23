4a09a0480683214715b17038cd984bac14c034be
cry0genic <cry0genic1103@gmail.com>
2021-03-23 17:56:19 +0530

    sos added

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -18,9 +18,27 @@ app.post('/user/image', auth, uploadImage)
 app.post('/user', auth, addUserDetails)
 app.get('/user', auth, getUserData)
 
-app.get('/globalChat', auth, globalChat) //do we need auth?
-app.get('/globalChat/:globalChatId', auth, globalChat1)
+app.get('/globalChat', auth, globalChat) 
+app.get('/globalChat/:globalChatId', auth, globalChat1) 
 app.post('/globalChat/:globalChatId/message', auth, messageGlobalChat)
 
 exports.api = functions.https.onRequest(app)
+
+exports.createSOS = functions.firestore.document('sos/{id}').onCreate((snapshot)=>{
+    return db.doc(`/users/`).get().then((docs)=>{
+        if (docs.exists){
+            return db.doc(`/sosSent/${snapshot.id}`).set({
+                createdAt : new Date().toISOString(),
+                recipient : docs.data(), //all users?
+                sender: snapshot.data().userHandle,
+                type: 'SOS',
+                read: false
+            })
+        }
+    }).then(()=>{
+        return;
+    }).catch((e)=>{
+        console.error(e)
+    })
+})
 
